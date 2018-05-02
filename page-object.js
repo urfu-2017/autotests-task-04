2b2fb5fc83b88fca23883068630f6478aecdb64f
Мишакова Наталья Сергеевна <mishakova.n@mail.ru>
2018-05-02 14:25:39 +0500

    Update page-object.js

--- page-object.js
+++ page-object.js
@@ -1,9 +1,14 @@
-// Здесь опиши используемые селекторы
-
 module.exports = {
     github: {
         login: '#login_field',
         password: '#password',
         signInBtn: '[name=commit]'
+    },
+    kilogram: {
+        loader: '.loader-page',
+        login_button: '.login-page__button',
+        menu: '.hamburger__box',
+        profile: '//span[2]',
+        name: '.profile__username'
     }
 };
