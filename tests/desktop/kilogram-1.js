5d7144237f969eb0890b8d2192c3c64fea1799b3
LuKas-03 <nur.kas99@ya.ru>
2018-05-03 00:36:00 +0500

    Написал E2E тест

--- tests/desktop/kilogram-1.js
+++ tests/desktop/kilogram-1.js
@@ -1,5 +1,22 @@
-describe.skip('Проверка профиля в K1logram:', () => {
+const assert = require('assert');
+
+describe('Проверка профиля в K1logram:', () => {
     it('должен получить информацию профиля', async function () {
-        // Напиши свой код здесь
+        const browser = this.browser;
+
+        await browser.url('https://k1logram.now.sh');
+        await browser.waitForVisible('.loader-page__spinner', 5000, true);
+        await browser.click('.login-page__button');
+        const pageUrl = await browser.getUrl();
+        if (pageUrl.indexOf('https://github.com/login/oauth/authorize') + 1) {
+            await browser.waitForEnabled('#js-oauth-authorize-btn', 3000, true);
+            await browser.click('#js-oauth-authorize-btn');
+        }
+        await browser.waitForVisible('.loader-page__spinner', 5000, true);
+        await browser.click('.hamburger__box');
+        await browser.click('.menu .menu__item:nth-child(2)');
+
+        const actualNickname = await browser.getText('.profile__username');
+        assert.equal(actualNickname, '@' + process.env.GH_LOGIN.toLowerCase());
     });
 });
