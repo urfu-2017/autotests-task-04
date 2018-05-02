12db3a12605fd4b202f636321c0697b2994fa5ad
vichkunina <vichkunina@bk.ru>
2018-05-02 19:30:35 +0500

    kilogram

--- tests/desktop/kilogram-1.js
+++ tests/desktop/kilogram-1.js
@@ -1,5 +1,22 @@
-describe.skip('Проверка профиля в K1logram:', () => {
+const assert = require('assert');
+const PO = require('../../page-object');
+
+describe('Проверка профиля в K1logram:', () => {
     it('должен получить информацию профиля', async function () {
-        // Напиши свой код здесь
+        const browser = this.browser;
+        const url = 'https://team7chat.now.sh/';
+
+        await browser.url(url);
+
+        const CurrentUrl = await browser.getUrl();
+        if (CurrentUrl.startsWith('https://github.com/')) {
+            await browser.waitForVisible(PO.kilogram.button, 5000);
+            await browser.click(PO.kilogram.button);
+        }
+
+        await browser.click(PO.kilogram.showProfile);
+
+        const nickname = await browser.getText(PO.kilogram.nickname);
+        assert(nickname === process.env.GH_LOGIN);
     });
 });
