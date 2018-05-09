4d7addd7b16fda2ef5af269060b01b17322281d5
Kobyakov Sergey <s.kobyakov@hostco.ru>
2018-05-09 12:24:30 +0500

    Сделал тест атомарным

--- tests/desktop/kilogram-1.js
+++ tests/desktop/kilogram-1.js
@@ -5,11 +5,22 @@ describe('Проверка профиля в K1logram:', () => {
     it('должен получить информацию профиля', async function () {
         const browser = this.browser;
 
-        await browser.url('https://the-best-messenger.now.sh/');
+        await browser.url(PO.kilogram.url);
 
         await browser.click(PO.kilogram.signInBtn);
+
+        const url = await browser.getUrl();
+        if (url.includes(PO.github.loginUrl)) {
+            await browser.setValue(PO.github.login, process.env.GH_LOGIN);
+            await browser.setValue(PO.github.password, process.env.GH_PWD);
+            await browser.click(PO.github.signInBtn);
+        }
+        if (url.includes(PO.github.loginOAuthUrl)) 
+            await browser.click(PO.github.loginOAuthUrl);
+
         await browser.click(PO.kilogram.profile);
-        const actual = await browser.getText(PO.kilogram.username);
+        const loginAndLink = await browser.getText(PO.kilogram.username);
+        const actual = loginAndLink[0];
 
         assert.equal(actual, process.env.GH_LOGIN);
     });
