cad550c23ccff0d05f8ef0c0261a4004af27ecec
d.fomin <d.fomin@skbkontur.ru>
2018-05-14 19:35:30 +0500

    review

--- tests/desktop/kilogram-1.js
+++ tests/desktop/kilogram-1.js
@@ -5,10 +5,24 @@ const profile = require('../../profile');
 describe('Проверка профиля в K1logram:', () => {
     it('должен получить информацию профиля', async function () {
         const browser = this.browser;
+		const onLoginPage = () => {
+			return browser.getUrl().includes('login?');
+		};
+		const onOauthPage = () => {
+			return browser.getUrl().includes('oauth');
+		};
 
         await browser.url('https://k1logram.now.sh/');
 		await browser.waitForVisible(PO.kilogram.loader, 5000, true);
 		await browser.click(PO.kilogram.login);
+		browser.waitUntil(function () {
+            return onLoginPage() || onOauthPage();
+		});
+		if (onLoginPage()) {
+			await browser.setValue(PO.github.login, process.env.GH_LOGIN);
+            await browser.setValue(PO.github.password, process.env.GH_PWD);
+            await browser.click(PO.github.signInBtn);
+		}
 		await browser.waitForVisible(PO.github.grantAccessButtonDisabled, 5000, true);
 		await browser.pause(2000);
 		
